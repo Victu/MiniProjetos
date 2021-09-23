46bed7dc396bcdb24c9d49793b024fc4c744e425
Victor Silva <victorsilva.lion@gmail.com>
2021-09-23 02:20:31 -0300

    Jokenpô
    Algumas pequenas alterações

--- jokenpo.py
+++ jokenpo.py
@@ -1,45 +1,53 @@
 from random import randint
 from time import sleep
+
 print('=-' * 25)
 print(f'{"> JOKENPÔ <":-^50}')
 print('-=' * 25)
-print('''PEDRA - Digite "1"
-PAPEL - Digite "2"
-TESOURA - Digite "3"''')
-opções = ('', 'PEDRA', 'PAPEL', 'TESOURA')
-pc = randint(1, 3)
-jogador = int(input('Selecione um número: '))
+opções = ('PEDRA', 'PAPEL', 'TESOURA')
+pc = randint(0, 2)
+nome = input('Seu nome/apelido: ').strip().capitalize()
 
-print('-' * 50)
-for o in range(len(opções)):
-    if jogador < 1 or jogador > 3:
+while True:
+    print('Digite "0" - PEDRA\nDigite "1" - PAPEL\nDigite "2" - TESOURA')
+    jogador = int(input(f'{nome}, selecione um número: '))
+    print('-' * 50)
+    if jogador < 0 or jogador > 2:
         print('ESCOLHA UMA OPÇÃO VÁLIDA!')
         break
-    if jogador == o:
-        print('JO', end='...  ')
-        sleep(0.8)
-        print('KEN', end='...  ')
-        sleep(0.8)
-        print('PÔ!!')
-        sleep(0.8)
-        print('=' * 35)
-        print(f'Você escolheu: {opções[o]}')
-    if pc == o:
-        print(f'O PC escolheu: {opções[o]}')
-        print('=' * 35)
-if jogador == pc:
-    print('Houve um empate!')
-else:
-    if jogador == 1 and pc == 3:
-        print('Muito bem! continue assim!')
-    elif jogador == 3 and pc == 1:
-        print('Ops... Não foi dessa vez...')
-    if jogador == 2 and pc == 1:
-        print('Parabéns! Você venceu.')
-    elif jogador == 1 and pc == 2:
-        print('Você perdeu...')
-    if jogador == 3 and pc == 2:
-        print('Boa!')
-    elif jogador == 2 and pc == 3:
-        print('Dessa vez, você perdeu... Tente novamente.')
+    print(f'{"JO":>16}', end='...  ')
+    sleep(0.8)
+    print('KEN', end='...  ')
+    sleep(0.8)
+    print('PÔ!!')
+    sleep(0.8)
+    print('-' * 50)
+    print(f'{nome} escolheu: {opções[jogador]}')
+    for o in range(len(opções)):
+        if pc == o:
+            print(f'O PC escolheu: {opções[o]}')
+            print('-' * 50)
+    print('- RESULTADO:', end=' ')
+    if jogador == pc:
+        print('Empate!')
+    else:
+        if jogador == 0 and pc == 2:
+            print('Você venceu! Continue assim.')
+        elif jogador == 2 and pc == 0:
+            print('Ops... Não foi dessa vez...')
+        if jogador == 1 and pc == 0:
+            print('Parabéns! Você venceu.')
+        elif jogador == 0 and pc == 1:
+            print('Você perdeu...')
+        if jogador == 2 and pc == 1:
+            print('Boa! Você venceu.')
+        elif jogador == 1 and pc == 2:
+            print('Boa sorte na próxima vez.')
+    print('-' * 50)
+    repetir = input('Quer repetir?[s/n] ').strip().lower()[0]
+    while not repetir in 'SsNn':
+        repetir = input('resposta inválida. Quer repetir?[s/n] ').strip().lower()[0]
+    if repetir in 'Nn':
+        break
+
 print('=-' * 25 + '\nFIM DE JOGO.')

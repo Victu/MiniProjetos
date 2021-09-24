076b7002797b7935efed03e7d27ac50bca3462b5
Victor Silva <victorsilva.lion@gmail.com>
2021-09-24 16:20:36 -0300

    Revert "Revert "Algumas pequenas modificações""
    This reverts commit 80c7176b6f459a1f7d104ee3884508c17c003dbe.

--- jokenpo.py
+++ jokenpo.py
@@ -7,47 +7,60 @@ print('-=' * 25)
 opções = ('PEDRA', 'PAPEL', 'TESOURA')
 pc = randint(0, 2)
 nome = input('Seu nome/apelido: ').strip().capitalize()
+vitória = derrota = 0
 
 while True:
+    print('=' * 50)
     print('Digite "0" - PEDRA\nDigite "1" - PAPEL\nDigite "2" - TESOURA')
-    jogador = int(input(f'{nome}, selecione um número: '))
+    print('=' * 50)
+    jogador = int(input(f'- {nome}, selecione um número: '))
+    while jogador < 0 or jogador > 2:
+        jogador = int(input('- Selecione uma opção válida: '))
+    print('- Agora é a vez do PC...')
+    sleep(1.5)
     print('-' * 50)
-    if jogador < 0 or jogador > 2:
-        print('ESCOLHA UMA OPÇÃO VÁLIDA!')
-        break
     print(f'{"JO":>16}', end='...  ')
-    sleep(0.8)
+    sleep(1)
     print('KEN', end='...  ')
-    sleep(0.8)
+    sleep(1)
     print('PÔ!!')
-    sleep(0.8)
+    sleep(1)
     print('-' * 50)
     print(f'{nome} escolheu: {opções[jogador]}')
     for o in range(len(opções)):
         if pc == o:
             print(f'O PC escolheu: {opções[o]}')
-            print('-' * 50)
     print('- RESULTADO:', end=' ')
     if jogador == pc:
         print('Empate!')
+        print('-' * 50)
     else:
         if jogador == 0 and pc == 2:
-            print('Você venceu! Continue assim.')
+            print('Boa! Você venceu.')
+            vitória += 1
         elif jogador == 2 and pc == 0:
             print('Ops... Não foi dessa vez...')
+            derrota += 1
         if jogador == 1 and pc == 0:
-            print('Parabéns! Você venceu.')
+            print(f'Parabéns, {nome}! Você venceu.')
+            vitória += 1
         elif jogador == 0 and pc == 1:
             print('Você perdeu...')
+            derrota += 1
         if jogador == 2 and pc == 1:
-            print('Boa! Você venceu.')
+            print(f'Muito bem, {nome}!')
+            vitória += 1
         elif jogador == 1 and pc == 2:
             print('Boa sorte na próxima vez.')
-    print('-' * 50)
+            derrota += 1
+        print('=' * 50)
+        print(f'{" PLACAR ":-^50}\n{"|PC:":>19} {derrota} x {nome}: {vitória}|')
+        print('=' * 50)
     repetir = input('Quer repetir?[s/n] ').strip().lower()[0]
     while not repetir in 'SsNn':
-        repetir = input('resposta inválida. Quer repetir?[s/n] ').strip().lower()[0]
+        repetir = input('Resposta inválida. Quer repetir?[s/n] ').strip().lower()[0]
     if repetir in 'Nn':
+        print('Finalizando...')
+        sleep(3)
         break
-
-print('=-' * 25 + '\nFIM DE JOGO.')
+print('=-' * 25 + f'\n{"FIM DE JOGO":>30}.')

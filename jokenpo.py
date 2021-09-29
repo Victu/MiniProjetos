eddcb1bcb510dadf7d92fb2cac2a67a59d2182a5
Victor Silva <victorsilva.lion@gmail.com>
2021-09-29 17:51:45 -0300

    Jokenpo v2.0
    - Houve algumas pequenas alterações.

--- jokenpo.py
+++ jokenpo.py
@@ -2,65 +2,65 @@ from random import randint
 from time import sleep
 
 print('=-' * 25)
-print(f'{"> JOKENPÔ <":-^50}')
+print(f'\033[1m{"> JOKENPÔ <":-^50}\033[m')
 print('-=' * 25)
-opções = ('PEDRA', 'PAPEL', 'TESOURA')
+opções = ('\033[1;36mPEDRA\033[m', '\033[1;34mPAPEL\033[m', '\033[1;35mTESOURA\033[m')
 nome = input('Seu nome/apelido: ').strip().capitalize()
 vitória = derrota = 0
 
-while True:
+while True:  # Começo
     print('=' * 50)
-    print('Digite "0" - PEDRA\nDigite "1" - PAPEL\nDigite "2" - TESOURA')
+    print(f'Digite "0" - {opções[0]}\nDigite "1" - {opções[1]}\nDigite "2" - {opções[2]}')
     print('=' * 50)
     jogador = int(input(f'- {nome}, selecione um número: '))
     pc = randint(0, 2)
     while jogador < 0 or jogador > 2:  # Entra nesse loop caso digite um número/opção inválida
-        jogador = int(input('- Selecione uma opção válida: '))
+        jogador = int(input('- \033[31mSelecione uma opção válida: \033[m'))
     print('- Agora é a vez do PC...')
     sleep(1.5)
     print('-' * 50)
-    print(f'{"JO":>16}', end='...  ')
+    print(f'\033[36m{"JO":>16}', end='...  \033[m')
     sleep(1)
-    print('KEN', end='...  ')
+    print('\033[34mKEN', end='...  \033[m')
     sleep(1)
-    print('PÔ!!')
+    print('\033[35mPÔ!!\033[m')
     sleep(1)
     print('-' * 50)
-    print(f'{nome} escolheu: {opções[jogador]}')  # Imprime a escolha do jogador
+    print(f'\033[33m{nome} escolheu:\033[m {opções[jogador]}')  # Imprime a escolha do jogador
     for o in range(len(opções)):
         if pc == o:
-            print(f'O PC escolheu: {opções[o]}')  # Imprime a escolha da máquina
-    print('- RESULTADO:', end=' ')
+            print(f'\033[33mO PC escolheu:\033[m {opções[o]}')  # Imprime a escolha da máquina
+    print('\033[33m- RESULTADO:\033[m', end=' ')
     if jogador == pc:
-        print('Empate!')
+        print('EMPATE!')
         print('-' * 50)
     else:  # Essa condição é iniciada quando a escolha do jogador é diferente da máquina
         if jogador == 0 and pc == 2:
-            print('Boa! Você venceu.')
+            print('\033[32mBoa! Você venceu.')
+            vitória += 1
+        elif jogador == 1 and pc == 0:
+            print(f'\033[32mParabéns, {nome}! Você venceu.')
+            vitória += 1
+        elif jogador == 2 and pc == 1:
+            print(f'\033[32mMuito bem, {nome}!')
             vitória += 1
         elif jogador == 2 and pc == 0:
-            print('Ops... Não foi dessa vez...')
+            print('\033[31mOps... Não foi dessa vez...')
             derrota += 1
-        if jogador == 1 and pc == 0:
-            print(f'Parabéns, {nome}! Você venceu.')
-            vitória += 1
         elif jogador == 0 and pc == 1:
-            print('Você perdeu...')
+            print('\033[31mVocê perdeu...')
             derrota += 1
-        if jogador == 2 and pc == 1:
-            print(f'Muito bem, {nome}!')
-            vitória += 1
         elif jogador == 1 and pc == 2:
-            print('Boa sorte na próxima vez.')
+            print('\033[31mBoa sorte na próxima vez.')
             derrota += 1
-        print('=' * 50)
-        print(f'{" PLACAR ":-^50}\n{"|PC:":>19} {derrota} x {nome}: {vitória}|')
+        print('\033[m=' * 50)
+        print(f'\033[1;33m{" PLACAR ":-^50}\033[m\n\033[33m{"|PC:":>19} {derrota} x {nome}: {vitória}|\033[m')
         print('=' * 50)
     repetir = input('Quer repetir?[s/n] ').strip().lower()[0]
     while not repetir in 'SsNn':  # Esse loop é iniciado quando resposta for inválida
         repetir = input('Resposta inválida. Quer repetir?[s/n] ').strip().lower()[0]
     if repetir in 'Nn':
-        print('Finalizando...')
+        print('SAINDO...')
         sleep(3)
         break
-print('=-' * 25 + f'\n{"FIM DE JOGO":>30}.')
+print('=-' * 25 + f'\033[1m\n{"FIM DE JOGO":>30}.\033[m')

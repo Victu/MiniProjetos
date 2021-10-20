56631c30115155c0d91efd4625cd850f04bb62eb
Victor Silva <victorsilva.lion@gmail.com>
2021-10-20 16:18:53 -0300

    Tabela do número da sorte
    O usuário irá digitar um número da sorte.
    - Se o número aparecer 1 vez na tabela, ganha 50. Se aparecer 2 vezes, recebe 100. E se aparecer +3 vezes, recebe 500.
    - Tem apenas 3 partidas.

--- Tabela da Sorte.py
+++ Tabela da Sorte.py
@@ -5,15 +5,16 @@ números = []
 print('=' * 70)
 print(f'<<{" TABELA DA SORTE ":-^66}>>')
 print('=' * 70)
-recompensa = total_arrecadado = sorteado = total_n = 0
-rodada = 3
 
-while True:  # Início
-    print(f'Rodadas: {rodada}')
+partidas = 3
+recebido = recompensa = total_arrecadado = sorteado = total_n = 0
+while True:  # Começa aqui
+    print(f'Partidas: {partidas}')
     n = int(input('- Escolha seu número da sorte entre \033[4m0\033[m e \033[4m300\033[m: '))
     while n < 0 or n > 300:  # Entra nessa condição, caso o número digitado seja inválido
         n = int(input('\033[31mNúmero inválido! \033[mEscolha entre \033[4m0\033[m e \033[4m300\033[m: '))
-    rodada -= 1
+    partidas -= 1
+    sleep(0.5)
 
     y_x = 14
     for y in range(0, y_x):  # Eixo Y
@@ -24,6 +25,7 @@ while True:  # Início
         if n in números:
             sorteado = n
         números.clear()
+
     # Criando a tabela
     print(f'<<{33 * "=-"}>>')
     for cl in range(len(tabela)):  # Colunas
@@ -37,32 +39,35 @@ while True:  # Início
     print(f'<<{33 * "-="}>>')
     sleep(2)
 
-    recebido = recompensa
+    recebido += recompensa
     if total_n == 1:
         recompensa = 50
     elif total_n == 2:
         recompensa = 100
     elif total_n >= 3:
         recompensa = 500
-    print(f'\033[1;32mParabéns! Seu número da sorte foi encontrado {total_n}x!\n'
-          f'De R${recebido:.2f} subiu para +R${recompensa:.2f} ↑\033[m'
-          if total_n > 0 else '\033[31m- NENHUM NÚMERO SORTEADO. BOA SORTE NA PRÓXIMA! :(\033[m')
-    total_arrecadado += recompensa
+    if total_n > 0:
+        total_arrecadado += recompensa
+        print(f'\033[1;32mParabéns! Seu número da sorte foi encontrado {total_n}x.\n'
+              f'De R${recebido:.2f} subiu para +R${recompensa:.2f} ↑\033[m')
+    else:
+        print('\033[31m- NENHUM NÚMERO SORTEADO. BOA SORTE NA PRÓXIMA! :(\033[m')
+    print('-' * 70)
 
     # Finalizando
-    if rodada == 0:
-        print(f'- RODADAS TERMINADA.')
+    if partidas == 0:
+        print(f'- SUAS PARTIDAS ACABARAM.')
         print('=' * 70)
         break
-    rept = input(f'Você ainda tem {rodada} rodada(s). Deseja repetir?[s/n] ')[0].strip()
-    while rept not in 'SsNn':
+    rept = input(f'Você ainda tem {partidas} partida(s). Deseja repetir?[s/n] ')[0].strip()
+    while rept not in 'SsNn':  # Entra nesse loop se a resposta não for válida
         rept = input('\033[31mDigite uma resposta válida.\033[m Deseja repetir?[s/n] ')[0].strip()
-    tabela.clear()
+    tabela.clear()  # Limpa toda a lista "tabela"
     total_n = 0
     print('=' * 70)
     if rept in 'Nn':
         break
 sleep(2.5)
-print(f'\033[1;7;33m {"VALOR TOTAL ARRECADADO:":>40} R${total_arrecadado:.2f}\t\t   '
-      f'           \033[m')
+print(f'\033[1;7;33m{"VALOR TOTAL ARRECADADO:":>40} R${total_arrecadado:<6.2f}'
+      f'                     \033[m')
 print('=' * 70)

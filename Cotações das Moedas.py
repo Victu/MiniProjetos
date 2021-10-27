57c5026161b9494f9b12ed673d39ee8bb94fac6a
Victor Silva <victorsilva.lion@gmail.com>
2021-10-27 17:14:39 -0300

    Cotação das Moedas
    Programa que mostra ao usuário, as cotações atualizadas das moedas USD, EUR e BTC

---
+++ Cotações das Moedas.py
@@ -0,0 +1,32 @@
+import requests
+from tkinter import *
+
+window = Tk()  # Inicia o Tkinter
+
+def pegar_cotacoes():
+    requisicao = requests.get("https://economia.awesomeapi.com.br/last/USD-BRL,EUR-BRL,BTC-BRL")
+
+    requisicao_dic = requisicao.json()
+
+    cotacao_dolar = requisicao_dic['USDBRL']['bid']
+    cotacao_euro = requisicao_dic['EURBRL']['bid']
+    cotacao_btc = requisicao_dic['BTCBRL']['bid']
+
+    texto = f'''
+    {"Dólar":>8}: {cotacao_dolar:<8}
+    {"Euro":>8}: {cotacao_euro:<8}
+    {"BTC":>8}: {cotacao_btc:<8}'''
+
+    texto_cotacao['text'] = texto
+
+window.title('Cotação Atual das Moedas')  # Título da janela
+window.geometry('310x200')  # Tamanho da inicialização da janela
+
+texto_info = Label(window, text='CLIQUE NO BOTÃO PARA ATUALIZAR AS COTAÇÕES.')
+texto_info.grid(column=0, row=0, padx=10, pady=10)  # Posição do texto
+botao = Button(window, text='Buscar cotações USD / EUR / BTC', command=pegar_cotacoes)
+botao.grid(column=0, row=2)  # Posição do botão
+texto_cotacao = Label(window, text='')
+texto_cotacao.grid(column=0, row=5, pady=12, padx=10)  # Posição da informação da cotação
+
+window.mainloop()  # Função para manter a janela aberta
